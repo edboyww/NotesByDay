0d5b76655ec5bb4bb1970aa47136710c6c3fa412
Ádám Vincze <edboyww@gmail.com>
2018-02-02 01:27:57 +0100

    Formatting the day string nicely and using a mindfuck hack for it

--- app/src/main/java/com/adamvincze/notesbyday/NbdApplication.java
+++ app/src/main/java/com/adamvincze/notesbyday/NbdApplication.java
@@ -1,8 +1,11 @@
 package com.adamvincze.notesbyday;
 
 import android.app.Application;
+import android.content.Context;
 
 import org.joda.time.LocalDate;
+
+import java.lang.ref.WeakReference;
 
 //Application override class to handle the global state of the chosen date
 public class NbdApplication extends Application {
@@ -10,20 +13,29 @@ public class NbdApplication extends Application {
     //The static variable for storing the actual chosen date
     private static LocalDate nbdDate = new LocalDate();
 
+    //Global Context for the sake of resolving resource strings in my helper class because fuck Android ecosystem overall at this point
+    //( Seriously, how fucked up is this? https://stackoverflow.com/questions/4391720/how-can-i-get-a-resource-content-from-a-static-context )
+    //Using a WeakReference to avoid memory leak
+    private static WeakReference<Context> nbdContext;
+
     @Override
     public void onCreate() {
         super.onCreate();
         nbdDate = LocalDate.now(); //Initializing the date
+        nbdContext = new WeakReference<Context>(this);
     }
 
-    //Getter
+    //Getter of the date
     public static LocalDate getNbdDate() {
         return nbdDate;
     }
 
-    //Setter
+    //Setter of the date
     public static void setNbdDate(LocalDate date) {
         nbdDate = date;
     }
+
+    //Getter of the Context, because fuck this
+    public static Context getNbdContext() { return nbdContext.get(); }
 
 }
--- app/src/main/java/com/adamvincze/notesbyday/NbdHelper.java
+++ app/src/main/java/com/adamvincze/notesbyday/NbdHelper.java
@@ -1,5 +1,7 @@
 package com.adamvincze.notesbyday;
 
+import android.content.Context;
+
 import org.joda.time.LocalDate;
 import org.joda.time.format.DateTimeFormat;
 
@@ -9,12 +11,13 @@ final class NbdHelper {
     //Formatting a Joda LocalDate object as <long local date>, <day of the week> or a day string
     static String nbdFormatDate(LocalDate date) {
         //TODO compare date to calendar and return day string accordingly (Today, Tomorrow, Yesterday, Day before yesterday)
-//        LocalDate comp = LocalDate.now();
-//        if (date.equals(comp)) return context.getString(R.string.today); else
-//        if (date.equals(comp.plusDays(1))) return context.getString(R.string.tomorrow);
-//        if (date.equals(comp.plusDays(2))) return context.getString(R.string.day_after_tomorrow);
-//        if (date.equals(comp.minusDays(1))) return context.getString(R.string.yesterday);
-//        if (date.equals(comp.minusDays(2))) return context.getString(R.string.day_before_yesterday);
+        Context context = NbdApplication.getNbdContext();
+        LocalDate comp = LocalDate.now();
+        if (date.equals(comp)) return context.getString(R.string.today); else
+        if (date.equals(comp.plusDays(1))) return context.getString(R.string.tomorrow);
+        if (date.equals(comp.plusDays(2))) return context.getString(R.string.day_after_tomorrow);
+        if (date.equals(comp.minusDays(1))) return context.getString(R.string.yesterday);
+        if (date.equals(comp.minusDays(2))) return context.getString(R.string.day_before_yesterday);
         return DateTimeFormat.forPattern("yyyy MMM d, EEE").print(date);
     }
 
