b57fc1ac27974ce21da50bc01218a5bbda91deef
Ádám Vincze <edboyww@gmail.com>
2018-03-13 16:25:51 +0100

    add missing converter file

---
+++ app/src/main/java/com/adamvincze/notesbyday/model/ConvertersForNoteDb.java
@@ -0,0 +1,32 @@
+package com.adamvincze.notesbyday.model;
+
+//Type converters the Joda time date fields
+
+import android.arch.persistence.room.TypeConverter;
+
+import org.joda.time.LocalDate;
+import org.joda.time.LocalDateTime;
+
+public class ConvertersForNoteDb {
+
+    @TypeConverter
+    public static LocalDate toDate(String value) {
+        return value == null ? null : new LocalDate(value);
+    }
+
+    @TypeConverter
+    public static String dateToString(LocalDate date) {
+        return date == null ? null : date.toString();
+    }
+
+    @TypeConverter
+    public static LocalDateTime toDateTime(String value) {
+        return value == null ? null : new LocalDateTime(value);
+    }
+
+    @TypeConverter
+    public static String datetimeToString(LocalDateTime dateTime) {
+        return dateTime == null ? null : dateTime.toString();
+    }
+
+}
